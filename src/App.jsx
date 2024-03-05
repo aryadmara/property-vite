91cc31c3b250d71b577c64266e6b0102d7e4e1da
aryadmara <158138683+aryadmara@users.noreply.github.com>
2024-03-05 16:32:46 +0700

    Component a (#2)
    * feat: component-a
    
    * feat: create views Floor, Footer, Hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './views/Hero'
+import Floor from './views/Floor'
+import Footer from "./views/Footer"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div>
-      <h1> Property Project Landing Page</h1>
+    <div className=' relative'>
+      <Navbar />
+      <Hero />
+      <Floor />
+      <Footer />
     </div>
-    </>
   )
 }
 
