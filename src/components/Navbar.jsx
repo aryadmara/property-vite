f3e6a5a54ef6567aadfaa218c50bac19f627a840
aryadmara <aryapoter@gmail.com>
2024-03-05 11:14:39 +0700

    feat: component-a

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (  
+        <header>
+        <h2 id="logo">D</h2>
+        <nav id="navbar">
+            <a href="#main-konten">Portofolio</a>
+            <a href="#about-me">About</a>
+            <a href="#main-konten-image">Artwork</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+    );
+}
+
+export default  Navbar;
