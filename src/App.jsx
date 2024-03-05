efec7ffd37690b02898db6f1609f1430a009104d
isaf123 <f.isa283@gmail.com>
2024-03-05 16:30:26 +0700

    feat: create section 3,6,7

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SectionPhoto from './views/section3'
+import SectionAmenities from './views/section6'
+import SectionCalculator from './views/section7'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>
-      <h1> Property Project Landing Page</h1>
-    </div>
+    <SectionCalculator/>
+    <SectionPhoto/>
+    <SectionAmenities/>
     </>
   )
 }
---
+++ src/views/section6.jsx
@@ -0,0 +1,33 @@
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+
+function SectionAmenities(){
+  return(
+    <>
+    <div className="py-16">
+      <div className="flex gap-3 items-center ">
+        <div className="border-b border-gray-400 w-72 h-1 "></div>
+        <div >
+            <p className=" w-62 text-[#3A8CB1] text-[2.3rem] font-medium">Photo Gallery</p>
+        </div>
+        <div></div>
+      </div>
+      <div className="flex flex-wrap px-72 gap-3 m-2">
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Beach Access</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">City Views</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Great School</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Heated Pool</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Large Kitchen</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Large Lot</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">New Construction</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Ocean Views</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Open Floor Plan</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Quiet and Private</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">Shopping Nearby</button>
+        <button className="py-1.5 px-3.5 bg-[#F4F4F4] rounded">SpaGated Community</button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default SectionAmenities
