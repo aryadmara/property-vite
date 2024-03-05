eac5aa449c372992c311a01f38343c368218261d
aryadmara <158138683+aryadmara@users.noreply.github.com>
2024-03-05 16:36:57 +0700

    feat: create section 3,6,7 (#3)
    Co-authored-by: isaf123 <f.isa283@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+import SectionPhoto from './views/section3'
+import SectionAmenities from './views/section6'
+import SectionCalculator from './views/section7'
+
 import Navbar from './components/Navbar'
 import Hero from './views/Hero'
 import Floor from './views/Floor'
 import Footer from "./views/Footer"
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <div className=' relative'>
       <Navbar />
       <Hero />
+      <SectionPhoto/>
       <Floor />
+    <SectionAmenities/>
+       <SectionCalculator/>
       <Footer />
     </div>
   )
