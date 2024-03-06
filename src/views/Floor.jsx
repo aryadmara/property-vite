8787f0d8cc1698d6f1dff0d73ad1c4a246197961
aryadmara <158138683+aryadmara@users.noreply.github.com>
2024-03-06 11:07:23 +0700

    feat: updated hover-hero, hover-floor, and add Map (#5)

--- src/views/Floor.jsx
+++ src/views/Floor.jsx
@@ -3,10 +3,10 @@ import floorPlan from "../assets/rumah.png"
 function Floor() {
     return ( 
         <div className="w-full h-[700px] border bg-[#f4f4f4] flex flex-col ">
-            <h1 className="text-4xl text-center m-16 text-[#486e81] ">Floor Plans</h1>
+            <h1 className="text-4xl text-center m-16 text-[#3A8CB1] ">Floor Plans</h1>
             <div className="flex justify-center border-b-2 border-t-2 py-3 mx-44">
-                <h1 className=" text-sm px-7 text-[#486e81]">FLOOR ONE</h1>
-                <h1 className=" text-sm px-7 text-[#c9c9c9]">FLOOR TWO</h1>
+                <h1 className=" text-sm px-7 text-[#3A8CB1] hover:text-[#3A8CB1]">FLOOR ONE</h1>
+                <h1 className=" text-sm px-7 text-[#c9c9c9] hover:text-[#3A8CB1]">FLOOR TWO</h1>
             </div>
                 <div className="flex justify-center m-5">
                 <img src={floorPlan} alt="" className="w-96 h-96" />
@@ -14,5 +14,6 @@ function Floor() {
         </div>
      );
 }
+// Membuat hover untuk teks views Floor
 
 export default Floor;
