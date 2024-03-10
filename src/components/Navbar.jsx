5624547d03c74de9ffc388e02eb947b49fa07def
aryadmara <158138683+aryadmara@users.noreply.github.com>
2024-03-10 13:52:16 +0700

    feat: add wa icon (#15)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { IoCloseCircleSharp } from "react-icons/io5";
 function Navbar() {
-
+    const [view, setView] = useState ("invisible md:visible")
+    // const [name, setName] = useState (`yaya toure`)
+    const handleHidden = () =>{
+        setView ("invisible md:invisible");
+        // setName (`Pogba`)
+    }
     return (  
         <header className=" absolute w-[80rem] m-auto left-0 right-0 my-7 flex items-center justify-between  text-white">
+            {/* <p>{name}</p> */}
+        <div className={`${view} text-black bottom-24 right-4 fixed bg-white shadow rounded-md z-50`} > 
+            <button className=" absolute -top-2 right-2 " type="button" onClick={handleHidden} ><IoCloseCircleSharp className=" text-red-600 fixed bg-white rounded-full" /></button>
+            <p className=" text-sm p-6 ">Hi!👋 <br />Kamu bisa menghubungi <br /> kami via WhatsApp</p>
+        </div>
+        <div className=" bottom-4 right-4 fixed bg-[#4eca5c] rounded-full p-4 z-50 cursor-pointer">
+            <span className="  rounded-full"><FaWhatsapp className="  w-8 h-8"/></span>
+        </div>
         <h2 className=" pl-4 md:pl-0 text-2xl cursor-pointer"><span className=" font-extrabold">Rocket</span>lister</h2>
         <nav className=" hidden md:block bg-opacity-100 text-xs ">
             <a className = " m-5 hover:text-[#c9c9c9]" href="#">OVERVIEW</a>
