e8a2b4c0e9bc438eaa282ba35c057aecaf262114
Adrian Irawan <150063940+VRay92@users.noreply.github.com>
2024-03-10 22:04:20 +0700

    fix: responsive section 2 & section 4 (#18)

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,7 +1,7 @@
 function VideoComponent() {
     return ( 
-        <div>
-        <iframe className="w-[46rem] h-[28rem]" src="https://youtube.com/embed/yGwRJBHbFKk?autoplay=1"  allowFullScreen title="room-tour"></iframe>
+        <div className="mx-3 md:w-[46rem] h-[20rem] md:h-[30rem] overflow-hidden">
+        <iframe className="w-[46rem] -ml-32 md:ml-0 h-full " src="https://youtube.com/embed/yGwRJBHbFKk?autoplay=1"  allowFullScreen title="room-tour"></iframe>
         </div>
      );
 }
