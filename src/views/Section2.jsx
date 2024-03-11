30b03985285c64f4a66b3b489f7432e7f6dae762
Adrian Irawan <150063940+VRay92@users.noreply.github.com>
2024-03-11 13:26:23 +0700

    Component e (#20)
    * fix: responsive section 2 & section 4
    
    * fix: edit margin section2
    
    * fix: update menu button section2 and responsive section8

--- src/views/Section2.jsx
+++ src/views/Section2.jsx
@@ -7,8 +7,11 @@ import { LuBath } from "react-icons/lu";
 import { GiMaze } from "react-icons/gi";
 import { IoLayersOutline } from "react-icons/io5";
 import { HiDotsHorizontal } from "react-icons/hi";
+import { useState } from "react";
 
 function Section2() {
+    const [onClick,SetOnClick] = useState(false)
+    
     return (
 
         <section className="flex flex-col md:flex-row items-center relative mt-[3.5rem]">
@@ -31,19 +34,31 @@ function Section2() {
                 </CardComponent>
                 
             </div>
-            <div id="right-section" className="p-4 mt-5 w-full md:w-1/2">
-                <div className="flex flex-col md:flex-row justify-center md:justify-between items-center">
-                <div className="flex md:block text-center md:text-left"><h1 className="text-4xl text-[#3A8CB1] font-bold w-[18rem]">About<br/>the Property</h1></div>
-                    <div className="flex pt-10 md:mt-0">
+
+            <div id="right-section" className="p-4 md-p-0 mt-5 md:mt-0 w-full md:w-1/2">
+                <div className="flex flex-col md:flex-row justify-center md:justify-between items-center mb-10">
+                    <div className="flex md:block text-center md:text-left"><h1 className="text-4xl text-[#3A8CB1] font-bold">About<br/>the Property</h1></div>
+                    <div className="flex pt-10 md:pt-0">
+
                         <BiSolidDollarCircle className="text-7xl text-[#BFB47A]" />
                         <h2 className="text-2xl">Offered at<br />$1,000,000</h2>
                     </div>
                 </div>
-      
+
                 <div className="mt-12 md:mt-0 ">
                     <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus est numquam cupiditate animi, iste inventore! Quas tempora quibusdam inventore sint exercitationem? Animi delectus nesciunt quibusdam assumenda error eius obcaecati nisi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus est numquam cupiditate animi, iste inventore! Quas tempora quibusdam inventore sint exercitationem? Animi delectus nesciunt quibusdam assumenda error eius obcaecati nisi.</p>
                 </div>
-                <div className="flex justify-end md:hidden"><HiDotsHorizontal /></div>
+                <div className="flex flex-col justify-end items-end md:hidden">
+                    
+                    <HiDotsHorizontal className="h-7 w-auto" onClick={() => SetOnClick(prevState => !prevState)}/>
+                    {onClick && <div className=""> 
+                        <button className="bg-[#f4f4f4] hover:bg-[#3A8CB1] hover:text-white shadow-lg w-40 h-8 text-xl align-middle font-bold flex justify-center items-center">Read More <SlArrowRight className="h-3" /></button>
+                        <button className="bg-[#f4f4f4] hover:bg-[#3A8CB1] hover:text-white shadow-lg w-40 h-8 gap-2 text-xl align-middle font-bold flex justify-center items-center "><BsDownload /> Brochure</button>
+                        <button className="bg-[#f4f4f4] hover:bg-[#3A8CB1] hover:text-white shadow-lg w-40 h-8 gap-2 text-xl align-middle font-bold flex justify-center items-center"><BsDownload /> Document</button>
+                    </div>}
+
+                </div>
+                     
                 <div className="hidden md:flex  justify-between font-bold mt-5">
                     <button className="h-8 text-xl align-middle font-bold flex justify-center items-center">Read More <SlArrowRight className="h-3" /></button>
                     <div className="flex gap-10">
--- src/views/Section8.jsx
+++ src/views/Section8.jsx
@@ -4,21 +4,21 @@ import { ImMail4 } from "react-icons/im";
 
 function Section8() {
     return (
-        <section className="flex h-[44rem]">
+        <section className="flex flex-col md:flex-row">
 
             <div className="">
-                <header className="text-3xl text-[#3A8CB1] mx-40 font-bold mb-12">Listing Presented by <br />Joe Agent</header>
-                <div id="photo-1" class="ml-40 w-[20rem] grid grid-cols-2 gap-4 mb-12">
+                <header className="text-3xl text-[#3A8CB1] ml-3 md:mx-40 font-bold mb-12">Listing Presented by <br />Joe Agent</header>
+                <div id="photo-1" className="ml-3 md:ml-40 w-[22rem] grid grid-cols-2 gap-4 mb-12">
                     <PhotoComponent url="https://as1.ftcdn.net/v2/jpg/03/33/86/90/1000_F_333869058_1H9me2PhuAGBtdxyjwjKj6N4XpEgFR56.jpg" className="object-left row-span-6 col-start-1 row-start-2 " />
                     <div id="biodata1">
                         <h1 className="col-start-2 row-start-2 h-10 text-2xl font-bold">Robert Luice</h1>
                         <p className="col-start-2 row-start-3 font-bold">Lead Broker</p>
                         <h1 className="col-start-2 row-start-4">Keller Williams</h1>
                         <div className="flex col-start-2 row-start-5 text-[#3A8CB1]">
-                            <FaWhatsapp classname="text-[#3A8CB1]" />480-255-0911
+                            <FaWhatsapp className="text-[#3A8CB1]" />480-255-0911
                         </div>
                         <div className="flex col-start-2 row-start-6 text-[#3A8CB1]">
-                            <ImMail4 classname="text-[#3A8CB1]" />joe@williams.com
+                            <ImMail4 className="text-[#3A8CB1]" />joe@williams.com
                         </div>
 
                         <p className="col-start-2 row-start-7">License number - RE1234567</p>
@@ -26,17 +26,17 @@ function Section8() {
                     </div>
                 </div>
 
-                <div id="photo-2" class="ml-40 w-[22rem] grid grid-cols-2 gap-4">
+                <div id="photo-2" className="ml-3 md:ml-40 w-[22rem] grid grid-cols-2 gap-4">
                     <PhotoComponent url="https://previews.123rf.com/images/nyul/nyul1102/nyul110200529/8782831-pretty-office-worker-girl-sitting-at-desk-looking-at-computer-screen-smiling.jpg" className="row-span-6 col-start-1 row-start-2  border-red-400" />
                     <div id="biodata2">
                         <h1 className="col-start-2 row-start-2 h-10 text-2xl font-bold">Annu Madera</h1>
                         <p className="col-start-2 row-start-3">Lead Broker</p>
                         <h1 className="col-start-2 row-start-4 font-bold">Keller Williams</h1>
                         <div className="flex col-start-2 row-start-5 text-[#3A8CB1]">
-                            <FaWhatsapp classname="text-[#3A8CB1]" />480-255-0911
+                            <FaWhatsapp className="text-[#3A8CB1]" />480-255-0911
                         </div>
                         <div className="flex col-start-2 row-start-6 text-[#3A8CB1]">
-                            <ImMail4 classname="text-[#3A8CB1]" />annu@madera.com
+                            <ImMail4 className="text-[#3A8CB1]" />annu@madera.com
                         </div>
 
                         <p className="col-start-2 row-start-7">License number - RE1234567</p>
@@ -45,30 +45,32 @@ function Section8() {
                 </div>
             </div>
 
-            <div id="right-section" className="relative -top-16 bg-[#f4f4f4c5] w-full h-[48rem] pt-[6rem] pl-20 ml-20">
-                <form className="flex flex-col">
-                    <h1 className="text-4xl text-[#3A8CB1] font-light pt-10 mb-10">Get in Touch</h1>
-                    
-                    <div className="flex gap-[16rem] mb-10 w-[40rem]">
-                        <label>First*</label>
-                        <label>Last*</label>
+            <div id="right-section" className="bg-[#f4f4f4c5] my-10 md:my-0 md:-mt-[20rem] w-fit md:w-full py-5 md:h-[62rem] pt-5 md:pt-[24rem] px-5 md:px-5 mx-3 md:mx-0 md:pl-20 md:ml-20">
+                <form className="flex flex-col w-fit md:w-[38rem] md:px-5">
+                    <h1 className="text-4xl text-[#3A8CB1] font-light pt-0 md:pt-10 mb-10">Get in Touch</h1>
+                    <div className="flex gap-5">
+                        <div className="flex flex-col w-6/12">
+                            <label className="">First*</label>
+                            <input type="text" name="first-name" className="bg-transparent outline-none border-black border-b w-full py-3"/>
+                        </div>
+                        <div className="flex flex-col w-6/12">
+                            <label className="">Last*</label>
+                            <input type="text" name="last-name" className="bg-transparent outline-none border-black border-b w-full py-3"/>
+                        </div>
                     </div>
-
-                    <div className="">
-                        <input type="text" name="first-name" className="bg-transparent outline-none border-black border-b w-[16rem] z-10"/>
-                        <input type="text" name="last-name" className="bg-transparent outline-none border-black border-b ml-10 w-[16.5rem] z-10 "/>
-                    </div>
-
                     <label className="my-3">Email*</label>
-                    <input type="text" name="email" className="bg-transparent outline-none border-black border-b w-[35rem] mt-3 z-10" />
+                    <input type="text" name="email" className="bg-transparent outline-none border-black border-b w-full mt-3 " />
 
                     <label className="my-3">Phone#</label>
-                    <input type="text" name="phone" className="bg-transparent outline-none border-black border-b w-[35rem] mt-3 z-10"  />
+                    <input type="text" name="phone" className="bg-transparent outline-none border-black border-b w-full mt-3"  />
 
                     <label className="my-3">Your Message</label>
-                    <input type="text" name="message" className="bg-transparent outline-none border-black border-b w-[35rem] mt-3 mb-6 z-10"  />
+                    <input type="text" name="message" className="bg-transparent outline-none border-black border-b w-full mt-3 mb-6"  />
 
-                    <div className="flex my-10"><button className="bg-[#3A8CB1] text-white w-40 h-10">Send Inquiry</button><button className="bg-[#3A8CB1] text-white ml-[15rem] w-40 h-10">Schedule a Tour</button></div>
+                    <div className="flex justify-between my-3 md:my-10">
+                        <button className="bg-[#3A8CB1] text-white w-32 md:w-40 h-10">Send Inquiry</button>
+                        <button className="bg-[#3A8CB1] text-white ml-[0rem] md:ml-[15rem] w-32 md:w-40 h-10">Schedule a Tour</button>
+                    </div>
 
                 </form>
 
